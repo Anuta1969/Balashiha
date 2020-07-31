a39c959a77265ff8cb15b6a4720394d91b8b3e1c
Anna <example@@gmail.com>
2020-07-31 18:31:28 +0300

    first

---
+++ js/script.js
@@ -0,0 +1,75 @@
+//modal 
+
+
+
+$('[data-modal=enter]').on('click', function() {
+ $('.overlay').fadeIn('slow');
+
+});
+
+$('.modal__close').on('click', function() {
+  $('.overlay, #enter').fadeOut('slow');
+ 
+ });
+
+
+
+ $('[data-modal2=enter]').on('click', function() {
+  $('.overlay2').fadeIn('slow');
+ 
+ });
+ 
+ $('.modal__close').on('click', function() {
+   $('.overlay2, #enter').fadeOut('slow');
+  
+  });
+
+
+
+//slider
+
+
+
+// $(document).ready(function(){
+//   $('.photos__slider').slick({
+   
+//      dots: true,
+//      arrows: false
+ 
+
+//   })
+ 
+  
+// });
+		
+
+
+
+
+  
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
