98603767dc55028d602e0a0f8e453bb90e6894f6
Anna <example@@gmail.com>
2020-08-03 01:07:22 +0300

    second

--- js/script.js
+++ js/script.js
@@ -2,45 +2,53 @@
 
 
 
-$('[data-modal=enter]').on('click', function() {
- $('.overlay').fadeIn('slow');
+// $('[data-modal=enter]').on('click', function() {
+//  $('.overlay').fadeIn('slow');
 
-});
-
-$('.modal__close').on('click', function() {
-  $('.overlay, #enter').fadeOut('slow');
- 
- });
-
-
-
- $('[data-modal2=enter]').on('click', function() {
-  $('.overlay2').fadeIn('slow');
- 
- });
- 
- $('.modal__close').on('click', function() {
-   $('.overlay2, #enter').fadeOut('slow');
-  
-  });
-
-
-
-//slider
-
-
-
-// $(document).ready(function(){
-//   $('.photos__slider').slick({
-   
-//      dots: true,
-//      arrows: false
- 
-
-//   })
- 
-  
 // });
+
+// $('.modal__close').on('click', function() {
+//   $('.overlay, #enter').fadeOut('slow');
+ 
+//  });
+
+
+
+//  $('[data-modal2=enter]').on('click', function() {
+//   $('.overlay2').fadeIn('slow');
+ 
+//  });
+ 
+//  $('.modal__close').on('click', function() {
+//    $('.overlay2, #enter').fadeOut('slow');
+  
+//   });
+
+
+
+
+
+
+
+$(document).ready(function(){
+  $('.photos__slider').slick({
+   
+     dots: true,
+     prevArrow: '<button type="button" class="slick-prev"><img src="icon/prev.png"></button>',
+     nextArrow: '<button type="button" class="slick-next"><img src="icon/next.png"></button>',
+     responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          arrows: false,
+        }
+      },
+    ]
+
+  })
+ 
+  
+});
 		
 
 
